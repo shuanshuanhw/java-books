d650bc02446b7bf6e8674acfc3753e820421c4d0
算算算 <oncwnuLskmjcCwtkAUZ0NMQtGiOc@git.weixin.qq.com>
2022-07-09 23:56:52 +0800

    20220709

---
+++ src/前端/简单的vue-cli的思路/testvue/src/views/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Index</h1>
+</template>
+<script>
+import { useRouter } from 'vue-router'
+
+export default {
+  setup() {
+    const router = useRouter()
+
+    // 也可以解构
+    const { push, go, back } = useRouter()
+  }
+}
+</script>
